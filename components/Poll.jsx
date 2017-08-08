1dc68a74347c052792fe94228ddc789e750b947d
Rahul Yedida <rahul@rahul-pc.rahul-pc>
2017-08-08 22:52:02 +0530

    Added chart to poll page

--- components/Poll.jsx
+++ components/Poll.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Chart from "chart.js";
+
 const $ = require("jquery");
 
 class Poll extends React.Component {
     constructor(props) {
         super(props);
         this.click = this.click.bind(this);
+        this.getRandomColor = this.getRandomColor.bind(this);
+    }
+
+    // from https://stackoverflow.com/a/25709983
+    getRandomColor() {
+        let letters = "0123456789ABCDEF".split("");
+        let color = "#";
+
+        for (let i = 0; i < 6; i++ )
+            color += letters[Math.floor(Math.random() * 16)];
+        return color;
     }
 
     click(id) {
@@ -14,11 +27,24 @@ class Poll extends React.Component {
 
         // Update the DB. For now, just update the mock array.
         // TODO: Later, update the DB instead of mock array.
-        this.props.polls[this.props.match.params.id].ops[id].votes += 1;
+        let pId = this.props.match.params.id;
+        this.props.polls[pId].ops[id].votes += 1;
 
         setTimeout(() => {
-            // TODO: Show the chart here.
-            console.log("Fire!");
+            let ctx = $("canvas");
+            new Chart(ctx, {
+                type: "doughnut",
+                data: {
+                    labels: this.props.polls[pId].ops.map(e => e.name),
+                    datasets: [{
+                        label: "# Votes",
+                        data: this.props.polls[pId].ops.map(e => e.votes),
+                        backgroundColor: this.props.polls[pId].ops.map(this.getRandomColor),
+                        borderWidth: 1
+                    }]
+                }
+            });
+
         }, 1000);
     }
 
@@ -37,6 +63,9 @@ class Poll extends React.Component {
                 </div>
                 <div className="col-md-4 col-md-offset-4">
                     {ops}
+                </div>
+                <div className="col-md-4 col-md-offset-4">
+                    <canvas width="100%" height="100%"></canvas>
                 </div>
             </div>
         );
